873d17ce3053f09a12b386a8b0950d83b1b58495
juancyrus <mragbadinoahkekeli@gmail.com>
2026-04-29 12:42:37 +0000

    nested loops

---
+++ 00pythonudemy/Construct number pattern.py
@@ -0,0 +1,16 @@
+# Write a Python program to construct the following pattern, using a nested loop number.
+# 1                                                                                                             
+# 22                                                                                                            
+# 333                                                                                                           
+# 4444                                                                                                          
+# 55555                                                                                                         
+# 666666                                                                                                        
+# 7777777                                                                                                       
+# 88888888                                                                                                      
+# 999999999  
+
+rows = 9
+for i in range(1, rows + 1):
+    for j in range(1, i + 1):
+        print(i, end=" ")
+    print()
